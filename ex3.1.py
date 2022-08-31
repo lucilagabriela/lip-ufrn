b239b84ae0286181f4ea85e9c76f8bd16ac09b35
Lucila Costa <lucilagdac@gmail.com>
2022-08-31 14:19:41 -0300

    up

---
+++ ex3.1.py
@@ -0,0 +1,5 @@
+names = ['lucila', 'gabriela', 'gomes', 'costa']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
---
+++ ex3.2.py
@@ -0,0 +1,2 @@
+names = ['lucila', 'gabriela', 'gomes', 'costa']
+print(f'My name is {names[0].title()} {names[1].title()} {names[2].title()} {names[3].title()}')
