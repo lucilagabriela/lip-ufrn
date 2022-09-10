9da096902fa295ff16c7736c237a2af56ee8af9b
[lucilagabriela] <lucilagdac@gmail.com>
2022-09-10 13:49:25 -0300

    up

---
+++ sala-de-aula/lista-alternada.py
@@ -0,0 +1,2 @@
+# esreva um programa que receba do usuário 2 listas de inteiros: uma lista a com m elementos e uma lista b com  n elementos, sendo que os elementos de c serão de a e b, inseridos de forma alternada.
+# exemplo: a = [1, 3, 5, 7], b = [2, 4], c = [1, 2, 3, 4, 5, 7]
---
+++ sala-de-aula/maior-frequencia.py
@@ -0,0 +1,6 @@
+# Utilizando abrangência, verifique qual valor aparece mais vezes em uma lista (de qualquer tipo). Imprima este(s) valor(es) e seu respectivo número de ocorrências. A lista será informada pelo usuário. Um número não poderá ser contato mais de uma vez.
+# Dica 1: Verifique se um valor está em uma lista
+# if valor in lista:
+#     do something
+# * not in = não está
+#Dica 2: lista.count(valor): conta ocorrência de um valor em uma lista
