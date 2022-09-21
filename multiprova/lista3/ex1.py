63f48b1ab7f14b6c0099c43f54c6cbcb45113090
Lucila Costa <lucilagdac@gmail.com>
2022-09-21 16:27:59 -0300

    Update ex1.py

--- multiprova/lista3/ex1.py
+++ multiprova/lista3/ex1.py
@@ -2,7 +2,7 @@ n = int(input(''))
 numeros = []
 for x in range(n):
     numeros.append(float(input()))
-# abrangencia:
+
 tetos = []
 for numero in numeros:
     if numero - int(numero) > 0:
@@ -11,8 +11,9 @@ for numero in numeros:
         teto = int(numero)
     numeros.append(numero)
 print(*tetos)
-
-
+# abrangencia:
+#tetos = [int(numero)+1 if numero - int(numero) > 0 else int(numero) for numero in numeros]
+#print(*tetos)
 
 #n = int(input(''))
 #numeros = []
@@ -27,3 +28,5 @@ print(*tetos)
 #  numeros.append(numero)
 
 #print(*numeros)
+
+# print(teto, end=' ')
